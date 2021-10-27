be84adac5299e0eb05a2890c739b18656196dc58
sandra9ermak <sandra1ermak@gmail.com>
2021-10-27 21:31:29 +0300

    6

--- src/friendsList/FriendsList.jsx
+++ src/friendsList/FriendsList.jsx
@@ -4,9 +4,9 @@ const FriendsList = ({ friends }) => {
     return(
     <ul>
         {friends.map(item =>
-            <li key={ item.id} className={item.isOnline ? s.isOnline : s.notOnline}>
-                <span>{item.isOnline}</span>
-            <img className={s.avatar} src={item.avatar} alt="" width="48" />
+            <li key={ item.id} className={s.listItem}>
+                <span className={item.isOnline ? s.isOnline : s.isOffline}>{item.isOnline}</span>
+            <img className={s.avatar} src={item.avatar} alt={item.name} width="48" height="48" />
             <p className={s.name}>{ item.name}</p>
         </li>
         )}
