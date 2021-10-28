510ab3d4a269ef8ac3cb26a199271fe06a8ad70b
sandra9ermak <sandra1ermak@gmail.com>
2021-10-28 10:30:18 +0300

    fin

--- src/friendsList/FriendsList.jsx
+++ src/friendsList/FriendsList.jsx
@@ -1,28 +1,13 @@
-import PropTypes from 'prop-types';
 import s from "./FriendsList.module.css";
+import FriendsListItem from './FriendsListItem';
 
 const FriendsList = ({ friends }) => {
     return(
-    <ul className={s.list}>
-        {friends.map(item =>
-            <li key={ item.id} className={s.listItem}>
-                <span className={item.isOnline ? s.isOnline : s.isOffline}>{item.isOnline}</span>
-            <img className={s.avatar} src={item.avatar} alt={item.name} width="48" height="48" />
-            <p className={s.name}>{ item.name}</p>
-        </li>
-        )}
+        <ul className={s.list}>
+            {friends.map(item =>
+                <FriendsListItem avatar={item.avatar} key={item.id} name={item.name} isOnline={ item.isOnline}/>
+                )}
     </ul>
 )}
 
 export default FriendsList;
-
-FriendsList.propTypes = {
-    friends:
-        PropTypes.arrayOf(
-            PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired
-  })).isRequired
-}
---
+++ src/friendsList/FriendsListItem.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import s from "./FriendsListItem.module.css";
+
+const FriendsListItem = ({ id, isOnline, avatar, name }) => {
+    return (
+    <li key={id} className={s.listItem}>
+                <span className={isOnline ? s.isOnline : s.isOffline}>{isOnline}</span>
+            <img className={s.avatar} src={avatar} alt={name} width="48" height="48" />
+            <p className={s.name}>{ name}</p>
+        </li>
+    )
+}
+
+export default FriendsListItem;
+
+FriendsListItem.propTypes = {
+    id: PropTypes.number,
+    isOnline: PropTypes.bool.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+}
